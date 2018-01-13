b8127f78f9ef245c3fe62c19ae54c89b1543bbf9
mohamde salem <drakelogan52@gmail.com>
2018-01-12 18:05:10 -0800

    added show, edit and update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,9 @@
 class TodosController < ApplicationController
 
+	def show
+		@todo = Todo.find(params[:id])
+	end
+
 	def new
 		@todo = Todo.new
 	end
@@ -7,15 +11,35 @@ class TodosController < ApplicationController
 	def create
 		@todo = Todo.new(todo_params)
 		if @todo.save
+			set_flash(:notice, "todo was created successfully")
 			redirect_to todo_path(@todo)
 		else
+			set_flash(:error, "Whoops, something went wrong please try again.")
 			render :new
 		end
 	end
 
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+
+	def update
+		@todo = Todo.find(params[:id])
+		if @todo.update(todo_params)
+			set_flash(:notice, "todo was updated successfully")
+			redirect_to todo_path(@todo)
+		else
+			set_flash(:error, "something went wrong please try again")
+			render :edit
+		end
+	end
 
 	private
 		def todo_params
 			params.require(:todo).permit(:name, :description)
+		end
+
+		def set_flash(type, msg)
+			flash[type] = msg
 		end
 end
